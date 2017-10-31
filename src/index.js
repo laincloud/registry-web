3a907ea4f02c43351a8a40a56b8ec6a1308c01db
bibaijin <bibaijin@gmail.com>
2017-10-31 15:50:19 +0800

    add progress

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import VueMaterial from 'vue-material';
 import VueRouter from 'vue-router';
 import 'vue-material/dist/vue-material.css';
 import infiniteScroll from 'vue-infinite-scroll';
+import hljs from 'highlightjs';
 
 import './favicon.ico';
 import App from './App.vue';
 import Repo from './components/Repo.vue';
 import Repos from './components/Repos.vue';
+
+hljs.initHighlightingOnLoad();
 
 Vue.use(VueMaterial);
 Vue.use(VueRouter);
